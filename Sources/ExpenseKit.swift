c093cf9b5d1f14e079309aed3913565f58617f44
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-09-27 21:49:44 +1000

    Add actual expense kit and tests from TTTTT

--- Sources/ExpenseKit.swift
+++ Sources/ExpenseKit.swift
@@ -1,4 +1,21 @@
-struct ExpenseKit {
+public typealias Value = Double
 
-    var text = "Hello, World!"
+public protocol ExpenseType {
+    var spent: Value { get }
+}
+
+public protocol BudgetType {
+    associatedtype Expense
+    var amount: Value { get }
+    var expenses: [Expense] { get }
+}
+
+public extension BudgetType where Self.Expense: ExpenseType {
+    var spent: Value {
+        return expenses.reduce(0) { sum, expense in sum + expense.spent  }
+    }
+
+    var left: Value {
+        return amount - spent
+    }
 }
--- Tests/ExpenseKitTests/ExpenseKitTests.swift
+++ Tests/ExpenseKitTests/ExpenseKitTests.swift
@@ -1,17 +1,48 @@
 import XCTest
-@testable import ExpenseKit
+import ExpenseKit
 
 class ExpenseKitTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(ExpenseKit().text, "Hello, World!")
+    struct Expense: ExpenseType {
+        let spent: Value
+    }
+
+    struct Budget: BudgetType {
+        let amount: Value
+        var expenses: [Expense] = []
+
+        init(amount: Value) {
+            self.amount = amount
+        }
+    }
+
+    var budget: Budget!
+    override func setUp() {
+        budget = Budget(amount: 100)
+    }
+
+    func testCreatingBudget() {
+        XCTAssertEqual(budget.spent, 0)
+        XCTAssertEqual(budget.amount, 100)
+        XCTAssertEqual(budget.left, 100)
+    }
+
+    func testAddingExpenses() {
+        budget.expenses.append(Expense(spent: 10))
+        XCTAssertEqual(budget.spent, 10)
+        XCTAssertEqual(budget.amount, 100)
+        XCTAssertEqual(budget.left, 90)
+
+        budget.expenses.append(Expense(spent: 20))
+        XCTAssertEqual(budget.spent, 30)
+        XCTAssertEqual(budget.amount, 100)
+        XCTAssertEqual(budget.left, 70)
     }
 
 
     static var allTests : [(String, (ExpenseKitTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("Creating budget", testCreatingBudget),
+            ("Adding expenses to budget", testAddingExpenses),
         ]
     }
 }
